4d288d2cc21fe1fd93e0026e1a8bdf14e5be98d4
lucidmoon <lucidmoonvillager@gmail.com>
2016-08-19 00:43:01 +0700

    update 18ags2016
    - add red-idle spritesheet
    - change player-1 display + debugBox
    - change score location

--- src/game.js
+++ src/game.js
@@ -5,19 +5,26 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'pongField', {preload: preload
 var bulat;
 var bulatSpeed_x = 5, bulatSpeed_y = 5;
 var skor, sekor=0;
+var red;
 
 function preload(){
     game.load.image('field', 'img/pongField-dummy.jpg');
-    game.stage.backgroundColor = '#ffffff';
+    game.load.atlasXML('red-idle', 'img/red/red-idle.png', 'img/red/red-idle.xml');
+    game.stage.backgroundColor = '#000000';
     
 }
             
 function create(){
     game.add.image(0, 0, 'field');
+    red = game.add.sprite(10,10, 'red-idle');
+    red.scale.x =  red.scale.y = .5;
+    red.animations.add('idle');
+    red.animations.play('idle',8,true);
     bulat = new Phaser.Circle(200, 200, 20);
-    player1 = new Phaser.Rectangle(30,30,15,70);
-    skor = game.add.text (50, 50, sekor);
-    skor.fontSize = 200;
+    //player1 = new Phaser.Rectangle(30,30,15,70);
+    player1 = new Phaser.Rectangle(red.x+90,red.y+10,10,100);
+    skor = game.add.text (300, 0, sekor);
+    skor.fontSize = 50;
 }
             
 function update(){
@@ -41,13 +48,15 @@ function update(){
     // =============== p1 ==================
     if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
         player1.y +=10;
+        red.y +=10;
     }
     else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)){
         player1.y -=10;
+        red.y -=10;
     }
     
     //check if hit by player1
-    if ((bulat.x >= player1.x+15) && (bulat.y >= player1.y) && (bulat.x <= player1.x+15) && (bulat.y <= player1.y+50)){
+    if ((bulat.x >= red.x+110) && (bulat.y >= red.y+10) && (bulat.x <= red.x+110) && (bulat.y <= red.y+110)){ //red.x+110, red-sprite_origin + red width + half_bulat = 100+10=110
         bulatSpeed_x *= -1;
     }
     
@@ -56,5 +65,5 @@ function update(){
             
 function render(){
     game.debug.geom(bulat, '#ffff00');
-    game.debug.geom(player1, '#ffff00');
+    game.debug.geom(player1, '#ff0000');
 }
